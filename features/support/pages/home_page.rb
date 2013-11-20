d3fdc73b4d46b7f0f40bf89bb6ffdb699320f968
Peter Neely <peter.neely@ctshirts.co.uk>
2013-11-20 15:58:22 +0000

    Trying to get tests to pass on build server

--- features/support/pages/home_page.rb
+++ features/support/pages/home_page.rb
@@ -14,7 +14,7 @@ class HomePage # < BasePage
   def set_locale(locale)
     $locale = locale.downcase!
     locale_promotion = data_for(:locale_ids)[$locale]
-    url = "#{FigNewton.base_url}/default.aspx?q=#{locale_promotion}|||||||||||||||"
+    url = "#{FigNewton.base_url}/default.aspx?q=#{locale_promotion}|||||||||||||||&changecountry=#{$locale}"
     navigate_to url
   end
 end
