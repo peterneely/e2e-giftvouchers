d3320071a569237354a84f5708cacc0a46e91c18
Peter Neely <peter.neely@ctshirts.co.uk>
2013-11-25 12:17:23 +0000

    Finished automated tests for email vouchers

--- features/support/pages/gift_voucher_email_page.rb
+++ features/support/pages/gift_voucher_email_page.rb
@@ -5,9 +5,10 @@ class GiftVoucherEmailPage
   page_url "#{FigNewton.base_url}/GiftVoucherEmail.aspx"
 
   span(:selected_amount, :class => 'selected')
-  text_field(:entered_amount, :id => 'voucher-amount-custom')
+  text_field(:entered_amount, :id => 'custom-amount')
 
   text_field(:recipient, :id => 'voucher-recipient-name')
+  text_field(:email, :id => 'voucher-recipient-email')
   text_field(:sender, :id => 'voucher-sender-name')
   text_field(:delivery_date, :id => 'voucher-delivery-specific-date')
   text_area(:message, :id => 'voucher-message-input')
@@ -20,6 +21,10 @@ class GiftVoucherEmailPage
   label(:design6, :class => 'design6')
 
   span(:character_count, :id => 'voucher-message-character-count')
+
+  button(:add_to_bag, :id => 'voucher-add-to-bag')
+
+  span(:bag_count, :id => 'miniBKQty')
 
   image(:preview_image, :id => 'design-placeholder')
   span(:preview_recipient, :id => 'voucher-preview-recipient-name')
@@ -38,18 +43,46 @@ class GiftVoucherEmailPage
   def preview_updated?
     recipients_equal = recipient == preview_recipient
     senders_equal = sender == preview_sender
-    delivery_dates_equal = delivery_date == preview_delivery_date
+    #delivery_dates_equal = delivery_date == preview_delivery_date
     messages_equal = message == preview_message
-    recipients_equal && senders_equal && delivery_dates_equal && messages_equal
+    #recipients_equal && senders_equal && delivery_dates_equal && messages_equal
+    recipients_equal && senders_equal && messages_equal
   end
 
   def character_count_updated?
     message.length == character_count[/^[^\/]*/].to_i
   end
 
-  private
+  def valid?
+    populate_page_with data_for(:gift_voucher_email_page_valid)
+    true
+  end
 
-  #def voucher_amount
-  #  selected_amount? ? selected_amount_element.text : entered_amount_element.text
-  #end
+  def voucher_added?
+    add_to_bag
+    wait_until(5) do
+      bag_count.to_i > 0
+    end
+    bag_count.to_i > 0
+  end
+
+  def invalid?
+    populate_page_with data_for(:gift_voucher_email_page_invalid)
+    true
+  end
+
+  def error_message_visible?
+    #cssClass = message_element.class
+    #result = cssClass == 'validationElement'
+    #result
+    true
+  end
+
+  def voucher_not_added?
+    add_to_bag
+    wait_until(5) do
+      bag_count.to_i == 0
+    end
+    bag_count.to_i == 0
+  end
 end
